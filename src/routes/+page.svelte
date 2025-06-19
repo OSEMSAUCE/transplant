86299b5cbb921c1a503a038e85dc922ccb34e37a
Chris Harris <christobinharris@gmail.com>
2025-06-19 08:20:46 -0700

    feat: add welcome page content and description of OSEM tools

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
-  import AccountMenu from '$lib/transferComponents/AccountMenu.svelte';
+	import AccountMenu from '$lib/transferComponents/AccountMenu.svelte';
 </script>
+
+<h2>Welcome to OSEM🤘🌲 Transplant App</h2>
+<br/>
+
+<h4>There will be a suite of tools for managing reforestation transparency here.</h4>
+<h4>Rn there is a CSV import tool, and project management tool called Transplant</h4>
+<br/>
+<h4>
+	For more info, check out the <a href="https://github.com/OSEMSAUCE/OSEM">OSEM🤘🌲 GitHub repo</a>
+</h4>
