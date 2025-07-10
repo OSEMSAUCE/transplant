87e1a1bbfba15e7c8329ad1ec350ed3ab575f4df
Chris Harris <christobinharris@gmail.com>
2025-07-10 12:03:01 -0700

    chore: remove commented out Prisma query code and cleanup page server file

--- src/routes/transplant/+page.server.ts
+++ src/routes/transplant/+page.server.ts
@@ -1,32 +1,7 @@
 import prisma from '$lib/server/prisma';
 
 // THIS IS WHERE THE QUERIES GO
-// async function main() {
-//     const Land = await prisma.land.findMany()
-//     const insertLand = await prisma.land.create({
-//         data: {
-//             landName: 'elsa@prisma.io',
-//             // projectId: null,
-//             hectares: 123,
-//             landHolder: "string" ,
-//             // polygonId: 'elsa@prisma.io',
-//             gpsLat: 123,
-//             gpsLon: 123,
-//             landNotes: "string",
-//         },
-//     })
-//     console.log(insertLand)
-// }
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect()
-//   })
-//   .catch(async (e) => {
-//     console.error(e)
-//     await prisma.$disconnect()
-//     process.exit(1)
-//   })
 
 export async function load() {
 	// A more direct approach to handle Prisma objects
